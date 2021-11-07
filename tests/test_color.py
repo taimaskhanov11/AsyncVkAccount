c427d45bc0786372fb2d9e3788069463b3e245e3
taimaskhanov11 <81189712+taimaskhanov11@users.noreply.github.com>
2021-11-07 05:33:46 +0300

    1.2.0.0
    delete tests and trash

--- tests/test_color.py
+++
@@ -1,9 +0,0 @@
-from more_termcolor import colors, colored
-from more_termcolor.colors import bold, brightgreen, yellow
-
-bold_text = colored('text', 'yellow')
-black = colors.blue('123')
-fancy = colors.brightblack(f'this whole string {black} including this {bold_text} fghjfgj')
-print(colors.brightyellow('asdfas'))
-
-print(fancy)
